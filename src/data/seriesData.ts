ecc81eb78b5989d6261a1cc8b042cf73423a1c66
Manzur Silviu <manzursilviu@yahoo.com>
2022-11-15 09:26:11 +0200

    chore(): initial commit

---
+++ src/data/seriesData.ts
@@ -0,0 +1,44 @@
+export const SERIES_LIST = [
+    {
+        title: "Games of Throne",
+        release: "2022-02-01",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+        votes: 9.2,
+        image: "https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?b=1&s=612x612&w=0&k=20&c=Dr-NNPW_DX02mXH2RsXOJ5bT0I7SfZ0qq_BTICqkM7Y="
+    },
+    {
+        title: "Prison Break",
+        release: "2022-04-09",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+        votes: 6.5,
+        image: "https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?b=1&s=612x612&w=0&k=20&c=Dr-NNPW_DX02mXH2RsXOJ5bT0I7SfZ0qq_BTICqkM7Y="
+    },
+    {
+        title: "Sons of Anarchy",
+        release: "2022-09-10",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+        votes: 9.9,
+        image: "https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?b=1&s=612x612&w=0&k=20&c=Dr-NNPW_DX02mXH2RsXOJ5bT0I7SfZ0qq_BTICqkM7Y="
+    },
+    {
+        title: "Manifest",
+        release: "2021-04-01",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+        votes: 8.5,
+        image: "https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?b=1&s=612x612&w=0&k=20&c=Dr-NNPW_DX02mXH2RsXOJ5bT0I7SfZ0qq_BTICqkM7Y="
+    },
+    {
+        title: "House of Gucci",
+        release: "2022-02-04",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+        votes: 7.5,
+        image: "https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?b=1&s=612x612&w=0&k=20&c=Dr-NNPW_DX02mXH2RsXOJ5bT0I7SfZ0qq_BTICqkM7Y="
+    },
+    {
+        title: "Peaky Blinders",
+        release: "2022-02-01",
+        description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.",
+        votes: 9.2,
+        image: "https://media.istockphoto.com/id/1244034031/vector/cinema-poster-with-cola-film-strip-and-clapper-vector.jpg?b=1&s=612x612&w=0&k=20&c=Dr-NNPW_DX02mXH2RsXOJ5bT0I7SfZ0qq_BTICqkM7Y="
+    },
+]
---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const COLOR_TEXT = "#262626"
+export const COLOR_GREEN = "#056C2E"
+export const COLOR_RED = "#A90000"
+
+export const BORDER_RADIUS_PX = 4
